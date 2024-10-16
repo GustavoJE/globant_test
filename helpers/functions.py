6c0b16ce3c8acc48dd12b60cc4ded4c3265f37d8
GustavJE <gechazu@minimalistech.com>
2024-10-16 10:48:47 -0300

    First commit, part 1 of challenge working

---
+++ helpers/functions.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np    
+
+def write_data(table, file, conn):
+       
+    for batch in pd.read_csv(file.file, header=None, chunksize=1000):
+        batch = batch.replace(np.nan, None)
+           
+        match table:
+            case "employees":
+                try:
+                    with conn.cursor() as cursor:
+                        for index, row in batch.iterrows():
+                            print(row.iloc[0], row.iloc[1], row.iloc[2], row.iloc[3], row.iloc[4])
+                            cursor.execute("INSERT INTO employees (id, name, datetime, department_id, job_id) VALUES (%s, %s, %s, %s, %s)", (row.iloc[0], row.iloc[1], row.iloc[2], row.iloc[3], row.iloc[4]))
+                            conn.commit()
+
+                except:
+                    print("Something went wrong :(")
+
+            case "departments":
+                try:
+                    with conn.cursor() as cursor:
+                        for index, row in batch.iterrows():
+                            print(row.iloc[0], row.iloc[1])
+                            cursor.execute("INSERT INTO departments (id, department) VALUES (%s, %s)", (row.iloc[0], row.iloc[1]))
+                            conn.commit()
+
+                except:
+                    print("Something went wrong :(")
+
+            case "jobs":
+                try:
+                    with conn.cursor() as cursor:
+                        for index, row in batch.iterrows():
+                            print(row.iloc[0], row.iloc[1])
+                            cursor.execute("INSERT INTO jobs (id, job) VALUES (%s, %s)", (row.iloc[0], row.iloc[1]))
+                            conn.commit()
+
+                except:
+                    print("Something went wrong :(")
+
+    print("Data inserted successfully")
+    
